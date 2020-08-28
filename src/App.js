a95595907e5bd30ec360f1d7cae4b0751359b975
phmaruf <phmaruf1@gamil.com>
2020-08-28 12:03:47 +0600

    project done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Country from './components/Country/Country';
+import Cart from './components/Cart/Cart';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [cart, setCart] = useState([]);
+  
+
+  useEffect(()=>{
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data =>setCountries(data))
+  }, [])
+  
+  const handleAddCountry = (country) => {
+    const newCart = [...cart, country];
+    setCart(newCart); 
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <h1>Country Loaded: {countries.length}</h1>
+      <h4>Country Added: {cart.length}</h4>
+      <Cart cart={cart}></Cart>
+      {
+        countries.map(country => <Country country={country} handleAddCountry={handleAddCountry} key={country.alpha3Code}></Country>)
+      }
+     <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
